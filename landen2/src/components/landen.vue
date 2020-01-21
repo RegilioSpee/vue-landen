3d7672b1a9e7ad7bed9703a5ba38ceb6d316efd2
RegilioSpee <contact@regiliospee.nl>
2020-01-21 20:04:33 +0100

    landen.vue ingevuld

---
+++ landen2/src/components/landen.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    <div v-bind:key="land.id" v-for="land in landen">
+        <LandItem v-bind:land="land"/>
+    </div>
+    </div>
+</template>
+
+<script>
+import LandItem from './LandItem.vue';
+
+export default {
+    name: "Landen",
+    components: {
+        LandItem
+    },
+    props: ["landen"]
+}
+</script>
+
+<style scoped>
+
+</style>
